538f8b5e7767ac2246e3d213f008d46e5a998dc0
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2019-11-20 13:41:55 +0100

    mer beskrivningar
    Men linux är kvar

--- subprocess FB.py
+++ subprocess FB.py
@@ -2,10 +2,22 @@
 import subprocess
 import os
 
-cmd = 'git --version'
+# kollar upp git version med returnvalue = 0
+#cmd = 'git --version'
+#returned_value = os.system(cmd) #tillkalar git commando
+#print('returned value:', returned_value)
 
-returned_value = os.system(cmd) #tillkalar git commando
-print('returned value:', returned_value)
+#kör 'echo' kommandon i shell i output
+#shell = subprocess.run(['echo', 'hello world'], capture_output=True, shell=True)
+#print(shell.stdout) 
 
-shell = subprocess.run(['echo', 'hello world'], capture_output=True, shell=True)
-print(shell.stdout) 
+# hämtar system info med returnvalue = 0
+#
+#cmd = "systeminfo"
+# 
+#returned_value = subprocess.call(cmd, shell=True)
+#print("returned_value:", returned_value) 
+
+
+wsl = subprocess.capita(['ls'])
+print(wsl)
