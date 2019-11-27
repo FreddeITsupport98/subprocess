138e146936ff66c13ee30292771b4786376ff050
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2019-11-27 09:46:06 +0100

    sub processor 2
    Nästa del är att lista ut en rätt kommando format för subprocessen

--- subprocess FB.py
+++ subprocess FB.py
@@ -1,6 +1,7 @@
 
 import subprocess
 import os
+import sys
 
 # kollar upp git version med returnvalue = 0
 #cmd = 'git --version'
@@ -18,6 +19,11 @@ import os
 #returned_value = subprocess.call(cmd, shell=True)
 #print("returned_value:", returned_value) 
 
+#ska väcka wsl kommando i windows
+#wsl = subprocess.call(['ls'])
+#print(wsl
 
-wsl = subprocess.capita(['ls'])
-print(wsl)
+print(hej vil )
+
+ws1 = subprocess.call(['net', 'user', 'Testuser', 'li05zzIe2', '/ADD'], shell=True)
+print(ws1) 
