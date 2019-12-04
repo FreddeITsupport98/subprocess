f78f280cede52e541d0b6f4f7d5fcbb839ce1420
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2019-12-04 11:30:18 +0100

    Lagt till fler koder
    Kommer forska och se hur dem har gjort med csv. filen

--- subprocess FB.py
+++ subprocess FB.py
@@ -3,6 +3,9 @@ import subprocess
 import os
 import sys
 
+#https://github.com/Z3roTwo/ADPythonAutomatisering?files=1
+
+
 # kollar upp git version med returnvalue = 0
 #cmd = 'git --version'
 #returned_value = os.system(cmd) #tillkalar git commando
@@ -23,12 +26,28 @@ import sys
 #wsl = subprocess.call(['ls'])
 #print(wsl
 
-print("hej vill du skapa ny användare?")
 
-input('ja', 'nej')
+#Skapar användare med administartör priviligeier
+#
+#print("hej vill du skapa ny användare? ja eller nej")
+#x = input()
+#if x == 'ja':
+#    ws1 = subprocess.call(['net', 'user', 'Testuser', 'li05zzIe2', '/ADD'], shell=True)
+#    print(ws1)
+#elif x == 'nej':
+#    exit
+    
+import random, string
 
-if a == 'ja':
-    ws1 = subprocess.call(['net', 'user', 'Testuser', 'li05zzIe2', '/ADD'], shell=True)
-    print(ws1)
-    elif:
-        exit
+chars = "abcdefghijklmnopqrstuvwxyz"
+chars1 = "1234567890"
+chars2 = "!#%&/()="
+password = ""
+for c in range(2):
+    password += random.choice(chars)
+    password += random.choice(chars.upper())
+    password += random.choice(chars1)
+    password += random.choice(chars2)
+password = ''.join(random.sample(password, len(password)))
+print(password)
+print(len(password))
